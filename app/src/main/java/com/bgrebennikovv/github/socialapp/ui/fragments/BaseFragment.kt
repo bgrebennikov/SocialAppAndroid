23fa698a399d80a3dc5408f67c67eec75a060329
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-02-07 16:50:39 +0400

    create Base fragment which get view bindings from params

---
+++ app/src/main/java/com/bgrebennikovv/github/socialapp/ui/fragments/BaseFragment.kt
@@ -0,0 +1,37 @@
+package com.bgrebennikovv.github.socialapp.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.viewbinding.ViewBinding
+import com.bgrebennikovv.github.socialapp.R
+
+class BaseFragment<T : ViewBinding>(
+    private val inflateMethod: (LayoutInflater, ViewGroup?, Boolean) -> T
+) : Fragment() {
+
+    val navHostMain by lazy {
+        childFragmentManager.findFragmentById(R.id.root_nav) as NavHostFragment
+    }
+
+    private var _binding : T? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = inflateMethod.invoke(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
