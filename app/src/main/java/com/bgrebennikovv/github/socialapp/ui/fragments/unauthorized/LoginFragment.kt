830c71d6ccbf57d89c82a83230af9b4d60199fcf
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-02-09 15:12:52 +0400

    add On Login Btn press function

--- app/src/main/java/com/bgrebennikovv/github/socialapp/ui/fragments/unauthorized/LoginFragment.kt
+++ app/src/main/java/com/bgrebennikovv/github/socialapp/ui/fragments/unauthorized/LoginFragment.kt
@@ -23,10 +23,6 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
         loginViewModel.getLoginResult().observe(viewLifecycleOwner, Observer {
             when (it.status) {
