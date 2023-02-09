b1c114f36fc68c4c7b47278c5a6753572ac0642a
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-02-09 15:20:43 +0400

    add new modules to DI

--- app/src/main/java/com/bgrebennikovv/github/socialapp/BaseApplication.kt
+++ app/src/main/java/com/bgrebennikovv/github/socialapp/BaseApplication.kt
@@ -1,8 +1,12 @@
 package com.bgrebennikovv.github.socialapp
 
 import android.app.Application
+import com.bgrebennikovv.github.socialapp.di.*
+import com.bgrebennikovv.github.socialapp.repository.UserApiRepository
+import com.bgrebennikovv.github.socialapp.repository.UserApiRepositoryImpl
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import org.koin.dsl.module
 
 internal class BaseApplication : Application() {
 
@@ -12,7 +16,16 @@ internal class BaseApplication : Application() {
         startKoin {
             androidContext(this@BaseApplication)
             modules(
-
+                ktorClientModule,
+                module {
+                    single<UserApiRepository> {
+                        UserApiRepositoryImpl()
+                    }
+                },
+                viewModelsModule,
+                useCasesModule,
+                localDataModule,
+                appSettingsModule
             )
         }
 
