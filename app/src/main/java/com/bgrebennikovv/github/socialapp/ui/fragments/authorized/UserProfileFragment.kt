49fe494ec5aaa004f983859b6d56fa4c4b98bbbd
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-02-09 18:22:51 +0400

    add empty on click listener for logout btn

--- app/src/main/java/com/bgrebennikovv/github/socialapp/ui/fragments/authorized/UserProfileFragment.kt
+++ app/src/main/java/com/bgrebennikovv/github/socialapp/ui/fragments/authorized/UserProfileFragment.kt
@@ -1,5 +1,7 @@
 package com.bgrebennikovv.github.socialapp.ui.fragments.authorized
 
+import android.os.Bundle
+import android.view.View
 import com.bgrebennikovv.github.socialapp.databinding.FragmentUserProfileBinding
 import com.bgrebennikovv.github.socialapp.ui.fragments.BaseFragment
 
@@ -8,6 +10,12 @@ class UserProfileFragment : BaseFragment<FragmentUserProfileBinding>(
     FragmentUserProfileBinding::inflate
 ) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        binding.logoutBtn.setOnClickListener {
+
+        }
+    }
 
 }
