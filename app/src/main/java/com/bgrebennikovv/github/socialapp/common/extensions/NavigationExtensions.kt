efe7c5013511f05aee2c14737652d7cfe32b7189
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-02-11 13:27:36 +0400

    add findRootNavController() extension

---
+++ app/src/main/java/com/bgrebennikovv/github/socialapp/common/extensions/NavigationExtensions.kt
@@ -0,0 +1,15 @@
+package com.bgrebennikovv.github.socialapp.common.extensions
+
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.bgrebennikovv.github.socialapp.R
+
+fun Fragment.findRootNavController() : NavController{
+    val navHost = requireActivity()
+        .supportFragmentManager
+        .findFragmentById(R.id.root_nav)
+    as NavHostFragment?
+    return navHost?.navController ?: findNavController()
+}
