24d5344b147a11fdf8bcad067e3adf9309b734e1
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-02-15 15:25:07 +0400

    rename LoginResponse to AuthResponse

--- app/src/main/java/com/bgrebennikovv/github/socialapp/useCases/LoginUserUseCase.kt
+++ app/src/main/java/com/bgrebennikovv/github/socialapp/useCases/LoginUserUseCase.kt
@@ -2,7 +2,7 @@ package com.bgrebennikovv.github.socialapp.useCases
 
 import com.bgrebennikovv.github.socialapp.data.models.login.BaseResponse
 import com.bgrebennikovv.github.socialapp.data.models.login.LoginRequest
-import com.bgrebennikovv.github.socialapp.data.models.login.LoginResponse
+import com.bgrebennikovv.github.socialapp.data.models.login.AuthResponse
 import com.bgrebennikovv.github.socialapp.repository.DataStoreRepository
 import com.bgrebennikovv.github.socialapp.repository.UserApiRepository
 import org.koin.core.component.KoinComponent
@@ -13,7 +13,7 @@ class LoginUserUseCase : KoinComponent {
     private val apiService : UserApiRepository by inject()
     private val dataStoreRepository : DataStoreRepository by inject()
 
-    suspend operator fun invoke(request: LoginRequest) : BaseResponse<LoginResponse>{
+    suspend operator fun invoke(request: LoginRequest) : BaseResponse<AuthResponse>{
         return apiService.login(request).also { data ->
             data.response?.let {
                 dataStoreRepository.setAccessToken(it.accessToken)
