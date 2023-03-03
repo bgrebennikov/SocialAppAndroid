9cf4d4c6239979e006a5f84ca6917235f7729ae0
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-03-03 23:20:24 +0400

    add ifPasswordsNotIdentical function

--- InputValidator/src/main/java/com/bgrebennikovv/github/inputvalidator/PasswordValidator.kt
+++ InputValidator/src/main/java/com/bgrebennikovv/github/inputvalidator/PasswordValidator.kt
@@ -1,28 +1,39 @@
 package com.bgrebennikovv.github.inputvalidator
 
+import android.text.Editable
+
 data class PasswordParams(
-    var minSize : Int = 8,
+    var minSize: Int = 8,
     var maxSize: Int = 128
 )
 
 class PasswordValidator(
-    private val password: String
+    private val password: String?,
 ) : ValidationResult() {
 
     var passwordParams = PasswordParams()
 
-    private fun hasErrors() : String?{
+    private fun hasErrors(): String? {
 
-        with(passwordParams){
+        if (password.isNullOrBlank()) return setError("Password must be not empty")
 
-            if(password.length < minSize || password.length > maxSize) return setError("Password must have more 12 symbols")
+        with(passwordParams) {
+
+            if (password.length < minSize || password.length > maxSize) return setError("Password must have more $minSize symbols")
 
         }
         return null
     }
 
-    infix fun onError(err: (String) -> Unit){
-        hasErrors()?.let{
+    fun ifPasswordsNotIdentical(p1: Editable?, p2: Editable?, callback: () -> Unit){
+        if(p1.toString() != p2.toString()) {
+            setError("Passwords must be identical")
+            callback.invoke()
+        }
+    }
+
+    infix fun onError(err: (String) -> Unit) {
+        hasErrors()?.let {
             err.invoke(it)
         }
     }
