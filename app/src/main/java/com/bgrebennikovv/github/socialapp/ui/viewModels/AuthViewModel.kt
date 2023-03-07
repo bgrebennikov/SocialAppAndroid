102fc2a3165e80c568c21ff7a624fe8baf62ba83
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-03-07 21:42:07 +0400

    complete signup logic

--- app/src/main/java/com/bgrebennikovv/github/socialapp/ui/viewModels/AuthViewModel.kt
+++ app/src/main/java/com/bgrebennikovv/github/socialapp/ui/viewModels/AuthViewModel.kt
@@ -3,52 +3,84 @@ package com.bgrebennikovv.github.socialapp.ui.viewModels
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.bgrebennikovv.github.socialapp.data.models.login.AuthResponse
-import com.bgrebennikovv.github.socialapp.data.models.login.BaseResponse
-import com.bgrebennikovv.github.socialapp.data.models.login.LoginRequest
+import com.bgrebennikovv.github.socialapp.data.models.login.*
 import com.bgrebennikovv.github.socialapp.useCases.LoginUserUseCase
+import com.bgrebennikovv.github.socialapp.useCases.SignUpEmailCheckUseCase
+import com.bgrebennikovv.github.socialapp.useCases.SignUpUserUseCase
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
-sealed class Event{
-    object GoNext : Event()
+sealed class SignUpViewPagerEvent {
+    object GoNext : SignUpViewPagerEvent()
+}
+
+sealed class EmailCheckEvent {
+    object OnSuccess: EmailCheckEvent()
+    object OnFailure: EmailCheckEvent()
 }
 
 class AuthViewModel(
-    private val loginUserUseCase: LoginUserUseCase
+    private val loginUserUseCase: LoginUserUseCase,
+    private val signUpUserUseCase: SignUpUserUseCase,
+    private val signUpEmailCheckUseCase: SignUpEmailCheckUseCase
 ) : ViewModel() {
 
-    private val loginResult : MutableLiveData<BaseResponse<AuthResponse>?> = MutableLiveData<BaseResponse<AuthResponse>?>()
-
+    private val authResult: MutableLiveData<BaseResponse<AuthResponse>?> =
+        MutableLiveData<BaseResponse<AuthResponse>?>()
+    
     var email: String? = null
     var firstName: String? = null
     var lastName: String? = null
     var password: String? = null
 
-    private val eventChannel = Channel<Event>(Channel.BUFFERED)
-    val eventsFlow = eventChannel.receiveAsFlow()
+    private val signUpViewPagerEventChannel = Channel<SignUpViewPagerEvent>(Channel.BUFFERED)
+    val signupViewPagerFlow = signUpViewPagerEventChannel.receiveAsFlow()
 
-    fun goNext(){
+    private val emailCheckEventChannel = Channel<EmailCheckEvent>(Channel.BUFFERED)
+    val emailCheckFlow = emailCheckEventChannel.receiveAsFlow()
+
+
+    fun goNext() {
         viewModelScope.launch {
-            eventChannel.send(Event.GoNext)
+            signUpViewPagerEventChannel.send(SignUpViewPagerEvent.GoNext)
         }
     }
 
     fun login(email: String, password: String) {
-        loginResult.value = BaseResponse.loading()
+        authResult.value = BaseResponse.loading()
         viewModelScope.launch {
 
-            loginResult.value = loginUserUseCase.invoke(
+            authResult.value = loginUserUseCase.invoke(
                 LoginRequest(
                     email, password
                 )
             )
-            loginResult.postValue(null)
+            authResult.postValue(null)
 
         }
     }
 
-    fun getLoginResult() = loginResult
+    fun checkEmailExists(email: String){
+        viewModelScope.launch {
+            val result = signUpEmailCheckUseCase.invoke(email)
+            if(result.response?.success == true) emailCheckEventChannel.send(EmailCheckEvent.OnSuccess)
+            else emailCheckEventChannel.send(EmailCheckEvent.OnFailure)
+        }
+    }
+
+    fun signUp() {
+        viewModelScope.launch {
+
+            authResult.value = signUpUserUseCase.invoke(
+                SignupRequest(
+                    email, password, firstName, lastName
+                )
+            )
+
+        }
+    }
+
+    fun getAuthResult() = authResult
 
 }
