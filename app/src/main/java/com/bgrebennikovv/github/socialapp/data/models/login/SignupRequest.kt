d79de9a6f89aa112f1b99f7a3e46d0c7caf28e45
Boris Grebennikov <bgrebennikovv@gmail.com>
2023-03-07 21:36:31 +0400

    make fields nullable

--- app/src/main/java/com/bgrebennikovv/github/socialapp/data/models/login/SignupRequest.kt
+++ app/src/main/java/com/bgrebennikovv/github/socialapp/data/models/login/SignupRequest.kt
@@ -1,8 +1,8 @@
 package com.bgrebennikovv.github.socialapp.data.models.login
 
 data class SignupRequest(
-    var email: String,
-    val password: String,
-    val firstName: String,
-    val lastName : String,
+    var email: String?,
+    val password: String?,
+    val firstName: String?,
+    val lastName : String?,
 )
